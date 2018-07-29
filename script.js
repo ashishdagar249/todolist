95a33776a657e4f50001b78ac1011c01ed83e376
ashishdagar249 <33806154+ashishdagar249@users.noreply.github.com>
2018-07-29 17:03:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+window.onload = function () {
+    let todos = []
+    let list = this.document.getElementById('list')
+    let newtask = this.document.getElementById('newtask')
+    let addtask = this.document.getElementById('addtask')
+    function retrieveTodos () {
+    if (localStorage['todos']) {
+    todos = JSON.parse(localStorage['todos'])
+    }
+    }
+    function saveTodos () {
+    localStorage['todos'] = JSON.stringify(todos)
+    }
+    function clearList () {
+    while(list.firstChild) {
+    list.removeChild(list.firstChild)
+    }
+    }
+    function createListItemFromTodo(todo, pos) {
+    let item = document.createElement('li')
+    let taskSpan = document.createElement('span')
+    let xBtn = document.createElement('button')
+    let upBtn = document.createElement('button')
+    let downBtn = document.createElement('button')
+    taskSpan.innerText = todo.task
+    xBtn.innerText = "X"
+    upBtn.innerText = "V"
+    downBtn.innerText = "^"
+    if (todo.done) {
+    taskSpan.style.textDecoration = 'line-through'
+    }
+    xBtn.onclick = function () {
+    todos[pos].done = !todos[pos].done
+    saveTodos()
+    refreshList()
+    }
+    upBtn.onclick = function () {
+        todos[pos].done=todos[pos-1].done + todos[pos].done
+            todos[pos].task=todos[pos-1].task + todos[pos].task
+
+            todos[pos-1].done=todos[pos].done - todos[pos-1].done
+            todos[pos-1].task=todos[pos].task - todos[pos-1].task
+
+            todos[pos].done=todos[pos].done - todos[pos-1].done
+            todos[pos].task=todos[pos].task - todos[pos-1].task
+
+            saveTodos()
+            refreshList()
+
+    }
+    item.setAttribute('data-id', pos)
+    item.appendChild(xBtn)
+    item.appendChild(upBtn)
+    item.appendChild(downBtn)
+    item.appendChild(taskSpan)
+    return item
+    }
+    function refreshList () {
+    retrieveTodos()
+    clearList()
+    for (let i = 0; i < todos.length; i++) {
+    list.appendChild(createListItemFromTodo(todos[i], i))
+    }
+    }
+    function addTodo () {
+    todos.push({
+    task: newtask.value,
+    done: false
+    })
+    saveTodos()
+    newtask.value = ""
+    refreshList()
+    }
+    addtask.onclick = addTodo
+    refreshList()
+    }
